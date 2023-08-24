7557792fe02e9e35bb0a9959149fb9031ba9514a
Shogo Takata <peshogo@gmail.com>
2023-08-24 11:10:14 +0900

    change to overlay

--- flake.nix
+++ flake.nix
@@ -1,23 +1,32 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: let 
-  pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in{
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlays.default ]; };
+    in
+    {
 
-    packages.x86_64-linux.espresso = pkgs.stdenv.mkDerivation rec {
-      name = "espresso";
-      version = "2.4";
-      src = pkgs.fetchFromGitHub {
-        owner = "chipsalliance";
-        repo = "espresso";
-        rev = "v${version}";
-        sha256 = "sha256-z5By57VbmIt4sgRgvECnLbZklnDDWUA6fyvWVyXUzsI=";
+      overlays.default = self: super: {
+        espresso =
+          pkgs.stdenv.mkDerivation rec {
+            name = "espresso";
+            version = "2.4";
+            src = pkgs.fetchFromGitHub {
+              owner = "chipsalliance";
+              repo = "espresso";
+              rev = "v${version}";
+              sha256 = "sha256-z5By57VbmIt4sgRgvECnLbZklnDDWUA6fyvWVyXUzsI=";
+            };
+
+            nativeBuildInputs = with pkgs;[ cmake ];
+          };
       };
 
-      nativeBuildInputs = with pkgs;[cmake];
-    };
+      packages.x86_64-linux.espresso = pkgs.espresso;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.espresso;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.espresso;
-  };
+      formatter.x86_64-linux = pkgs.nixpkgs-fmt;
+
+    };
 }
